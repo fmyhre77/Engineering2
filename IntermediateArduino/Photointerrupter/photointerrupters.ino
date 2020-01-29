701c528f440c0fbd26504dc3253e1fea761edd2b
fmyhre77 <56264859+fmyhre77@users.noreply.github.com>
2020-01-29 09:51:25 -0500

    Adding comments

--- IntermediateArduino/Photointerrupter/photointerrupters.ino
+++ IntermediateArduino/Photointerrupter/photointerrupters.ino
@@ -1,11 +1,14 @@
-int ledPin = 10;
-int interruptPin = 2;
+//Photointerrupters
+//Frank Myhre
+//Use a T-Slot photointerrupter to detect if the pathway is blocked by making a light flash
+int ledPin = 10; //LED pin
+int interruptPin = 2; //Photointerrupter pin
 int state = LOW;
 int counter = 0;
  
 void setup() {
   pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
+  pinMode(interruptPin, INPUT_PULLUP); //pulling up pin
   attachInterrupt(0, blink, RISING);
   attachInterrupt(1, off, FALLING);
   Serial.begin(9600);
@@ -13,17 +16,17 @@ void setup() {
  
 void loop() {
   digitalWrite(ledPin, state);
-  Serial.print("counter is: ");
-  Serial.println(counter);
+  Serial.print("counter is: "); //print counter for how many times the light went off
+  Serial.println(counter);//printing counter
 }
  
 void blink() {
   state = !state;
-  counter++;
+  counter++;//adding to counter value
   digitalWrite(ledPin, HIGH);
 }
  
 void off() {
   state = !state;
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW); // turns off LED
  }
